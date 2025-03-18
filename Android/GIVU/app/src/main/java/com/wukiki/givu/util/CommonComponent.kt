646e02ba66572fb523ea01216f54c169c5272b32
Jang Hong Jun <jhjun99421@gmail.com>
2025-03-18 14:53:00 +0900

    [Android/Feat] 펀딩 생성하기 첫 화면 UI 구현
    done: S12P21D107-49

--- Android/GIVU/app/src/main/java/com/wukiki/givu/util/CommonComponent.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/util/CommonComponent.kt
@@ -1,18 +1,25 @@
 package com.wukiki.givu.util
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,6 +58,34 @@ fun CommonTopBar() {
             Icon(painter = painterResource(R.drawable.ic_home), null)
         }
 
+    }
+}
+
+
+@Composable
+fun CommonBottomButton(modifier: Modifier) {
+    Box(
+        modifier = modifier
+    ) {
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+            enabled = true,
+            shape = RoundedCornerShape(5.dp),
+            border = BorderStroke(1.dp, Color(0xFFECECEC)),
+            colors = ButtonDefaults.buttonColors(colorResource(R.color.main_primary)),
+            elevation = ButtonDefaults.elevation(0.dp)
+            ) {
+            Text(
+                text = "선물 선택하기",
+                fontFamily = suit,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                color = Color.White
+            )
+        }
     }
 }
 
