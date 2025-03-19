dea6f7e26807c29504e5e62cab57d40f79cb3eb1
정지원 <littlesam95@naver.com>
2025-03-19 10:13:32 +0900

    [Android/feat] 화면 전환하기 S12P21D107-68
     - 비로그인 상태에서 홈 화면에서 로그인 화면으로 이동 가능

--- Android/GIVU/app/src/main/java/com/wukiki/givu/views/home/HomeAppBarPager.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/views/home/HomeAppBarPager.kt
@@ -10,16 +10,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.TopAppBar
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.wukiki.domain.model.User
 import com.wukiki.givu.R
+import com.wukiki.givu.ui.suit
 
 @Composable
-fun HomeAppBarPager() {
+fun HomeAppBarPager(navController: NavController, user: User?) {
     TopAppBar(
         backgroundColor = Color.White,
         contentColor = Color.Black,
@@ -40,14 +46,29 @@ fun HomeAppBarPager() {
                     .height(16.dp)
             )
 
-            Icon(
-                painter = painterResource(id = R.drawable.ic_notification),
-                contentDescription = "Notifications",
-                tint = Color.Red,
-                modifier = Modifier
-                    .size(28.dp)
-                    .clickable { /* TODO: 알림 클릭 이벤트 */ }
-            )
+            when (user == null) {
+                true -> {
+                    Text(
+                        text = "로그인",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = suit,
+                        color = Color.Red,
+                        modifier = Modifier.clickable { navController.navigate(R.id.action_home_to_login) }
+                    )
+                }
+
+                else -> {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_notification),
+                        contentDescription = "Notifications",
+                        tint = Color.Red,
+                        modifier = Modifier
+                            .size(28.dp)
+                            .clickable { /* TODO: 알림 클릭 이벤트 */ }
+                    )
+                }
+            }
         }
     }
 }
--- Android/GIVU/app/src/main/java/com/wukiki/givu/views/home/HomeScreen.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/views/home/HomeScreen.kt
@@ -6,17 +6,24 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import com.wukiki.domain.model.Funding
 import com.wukiki.givu.R
 import com.wukiki.givu.ui.suit
+import com.wukiki.givu.views.home.viewmodel.HomeViewModel
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(homeViewModel: HomeViewModel = hiltViewModel(), navController: NavController) {
+    val user by homeViewModel.user.collectAsState()
+
     val sampleFundings = listOf(
         Funding(
             id = "1",
@@ -214,7 +221,7 @@ fun HomeScreen() {
         modifier = Modifier.fillMaxWidth()
     ) {
         item {
-            HomeAppBarPager()
+            HomeAppBarPager(navController, user)
             Spacer(modifier = Modifier.height(4.dp))
             SearchBarItem("웅") { }
         }
@@ -228,15 +235,17 @@ fun HomeScreen() {
             )
             PopularFundingListPager(sampleFundings)
         }
-        item {
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = stringResource(id = R.string.text_my_friends_funding),
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = suit
-            )
-            FriendFundingListPager(sampleFundings)
+        if (user != null) {
+            item {
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = stringResource(id = R.string.text_my_friends_funding),
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = suit
+                )
+                FriendFundingListPager(sampleFundings)
+            }
         }
         item {
             Spacer(modifier = Modifier.height(16.dp))
