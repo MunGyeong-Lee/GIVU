84d27c823178f32d0e4fb03d204fb39aa65f5532
정지원 <littlesam95@naver.com>
2025-03-19 10:18:09 +0900

    [Android/feat] 로그인 처리 S12P21D107-68
     - 서버와의 통신 준비
     - 로그인 성공 시 DataStore를 활용하여 사용자의 정보를 로컬에 저장함

--- Android/GIVU/data/src/main/java/com/wukiki/data/repository/auth/AuthRemoteDataSourceImpl.kt
+++ Android/GIVU/data/src/main/java/com/wukiki/data/repository/auth/AuthRemoteDataSourceImpl.kt
@@ -1,10 +1,18 @@
 package com.wukiki.data.repository.auth
 
 import com.wukiki.data.api.AuthApi
+import com.wukiki.data.entity.UserEntity
+import okhttp3.RequestBody
+import retrofit2.Response
 import javax.inject.Inject
 
 class AuthRemoteDataSourceImpl @Inject constructor(
     private val authApi: AuthApi
 ) : AuthRemoteDataSource {
 
+    override suspend fun loginWithKakao(
+        accessToken: String,
+        body: RequestBody
+    ): Response<UserEntity> =
+        authApi.loginWithKakao(accessToken, body)
 }
--- Android/GIVU/data/src/main/java/com/wukiki/data/repository/auth/AuthRepositoryImpl.kt
+++ Android/GIVU/data/src/main/java/com/wukiki/data/repository/auth/AuthRepositoryImpl.kt
@@ -1,10 +1,32 @@
 package com.wukiki.data.repository.auth
 
+import com.wukiki.data.mapper.UserMapper
+import com.wukiki.domain.model.ApiResult
+import com.wukiki.domain.model.User
 import com.wukiki.domain.repository.AuthRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.RequestBody
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val authRemoteDataSource: AuthRemoteDataSource
 ) : AuthRepository {
 
+    override suspend fun loginWithKakao(accessToken: String, body: RequestBody): ApiResult<User> =
+        try {
+            val response = withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
+                authRemoteDataSource.loginWithKakao(accessToken, body)
+            }
+
+            val responseBody = response.body()
+            if (response.isSuccessful && (responseBody != null)) {
+                ApiResult.success(UserMapper(responseBody))
+            } else {
+                ApiResult.error(response.errorBody().toString(), null)
+            }
+        } catch (e: Exception) {
+            ApiResult.fail()
+        }
 }
