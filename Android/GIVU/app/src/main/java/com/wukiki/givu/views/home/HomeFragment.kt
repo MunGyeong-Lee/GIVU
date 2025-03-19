1d4e6015ea3d69eeadda4d34660945e757a72444
정지원 <littlesam95@naver.com>
2025-03-19 10:13:32 +0900

    [Android/feat] 화면 전환하기 S12P21D107-68
     - 비로그인 상태에서 홈 화면에서 로그인 화면으로 이동 가능

--- Android/GIVU/app/src/main/java/com/wukiki/givu/views/home/HomeFragment.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/views/home/HomeFragment.kt
@@ -2,19 +2,35 @@ package com.wukiki.givu.views.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.wukiki.givu.R
 import com.wukiki.givu.config.BaseFragment
 import com.wukiki.givu.databinding.FragmentHomeBinding
+import com.wukiki.givu.views.MainViewModel
+import com.wukiki.givu.views.home.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
+    private val viewModel: HomeViewModel by viewModels()
+    private val mainViewModel: MainViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.vm = viewModel
+
         binding.composeHome.setContent {
-            HomeScreen()
+            HomeScreen(navController = findNavController())
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        mainViewModel.setBnvState(true)
     }
 }
