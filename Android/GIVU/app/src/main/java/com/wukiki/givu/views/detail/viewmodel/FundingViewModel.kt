ec15962e8a672ca050883b7b9bcb5414fdbc0334
Jang Hong Jun <jhjun99421@gmail.com>
2025-03-21 15:06:21 +0900

    [Android/Feat] 가격 자료형 변경 및 천단위 콤마 적용 S12P21D107-80

--- Android/GIVU/app/src/main/java/com/wukiki/givu/views/detail/viewmodel/FundingViewModel.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/views/detail/viewmodel/FundingViewModel.kt
@@ -32,7 +32,7 @@ class FundingViewModel @Inject constructor(
             categoryName = "",
             scope = "전체 공개",
             participantsNumber = "12",
-            fundedAmount = "1,200원",
+            fundedAmount = 1200,
             status = "진행중",
             images = listOf(
                 "https://img.tumblbug.com/eyJidWNrZXQiOiJ0dW1ibGJ1Zy1pbWctYXNzZXRzIiwia2V5IjoiY292ZXIvNzMyZWNiODgtMDdhYS00Y2FmLThlNTEtNGIwOTkyMDY1MzJmL2U3N2MwYTEzLTE5NzktNDJlNy1hNDFjLTljN2JmMTIxZGMzNC5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjQ2NSwiaGVpZ2h0Ijo0NjUsIndpdGhvdXRFbmxhcmdlbWVudCI6dHJ1ZX19fQ==",
