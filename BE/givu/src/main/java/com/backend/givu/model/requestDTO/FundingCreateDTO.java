88577b6397bac5e6423053c20f274f382c01f5b3
이문경 <moon943380@gmail.com>
2025-03-25 12:58:14 +0900

    [BE/feat]펀딩생성하기 ë§만드는 중

---
+++ BE/givu/src/main/java/com/backend/givu/model/requestDTO/FundingCreateDTO.java
@@ -0,0 +1,21 @@
+package com.backend.givu.model.requestDTO;
+
+import com.backend.givu.model.Enum.FundingsCategory;
+import com.backend.givu.model.Enum.FundingsScope;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FundingCreateDTO {
+    private String title;
+    private int userId;
+    private int productId;
+    private String body;
+    private String description;
+    private String category;
+    private String categoryName;
+    private FundingsScope scope;
+
+}
