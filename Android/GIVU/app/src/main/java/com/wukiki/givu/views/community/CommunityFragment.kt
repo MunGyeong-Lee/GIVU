e86d0ab6ce9ae3c363fc8f13422f83c42b0e1e46
정지원 <littlesam95@naver.com>
2025-03-26 14:12:24 +0900

    [Android/feat] 커뮤니티 화면 구현 S12P21D107-105
     - 전체 선택 시 최근 참여가 이루어진 펀딩, 내 친구들이 참여한 펀딩, 내 친구들이 생성한 펀딩 보여주기
     - 특정 친구 선택 시 해당 친구가 참여한 펀딩과 생성한 펀딩 보여주기

--- Android/GIVU/app/src/main/java/com/wukiki/givu/views/community/CommunityFragment.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/views/community/CommunityFragment.kt
@@ -2,16 +2,30 @@ package com.wukiki.givu.views.community
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.wukiki.givu.R
 import com.wukiki.givu.config.BaseFragment
 import com.wukiki.givu.databinding.FragmentCommunityBinding
+import com.wukiki.givu.views.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CommunityFragment : BaseFragment<FragmentCommunityBinding>(R.layout.fragment_community) {
 
+    private val mainViewModel: MainViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.composeCommunity.setContent {
+            CommunityScreen(navController = findNavController())
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        mainViewModel.setBnvState(true)
     }
 }
