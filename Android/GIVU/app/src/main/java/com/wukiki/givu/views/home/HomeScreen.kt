2cde59f49806bc73ed170af41987f00fcc0fb0b5
Jang Hong Jun <jhjun99421@gmail.com>
2025-03-26 10:28:42 +0900

    [Android/Feat] 홈화면 플로팅 액션 버튼 추가 S12P21D107-106

--- Android/GIVU/app/src/main/java/com/wukiki/givu/views/home/HomeScreen.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/views/home/HomeScreen.kt
@@ -1,14 +1,28 @@
 package com.wukiki.givu.views.home
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -202,46 +216,81 @@ fun HomeScreen(homeViewModel: HomeViewModel = hiltViewModel(), navController: Na
         )
     )
 
-    LazyColumn(
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        item {
-            HomeAppBarPager(navController, user)
-            Spacer(modifier = Modifier.height(4.dp))
-            SearchBarItem(navController)
+    Scaffold(
+        floatingActionButton = {
+            Column(horizontalAlignment = Alignment.End) {
+
+                FloatingActionButton(
+                    onClick = {
+
+                    },
+                    backgroundColor = colorResource(id = R.color.main_primary),
+                    modifier = Modifier
+                        .padding(bottom = 20.dp)
+                        .border(
+                            (0.5).dp, Color(0xFFBFE0EF),
+                            RoundedCornerShape(30.dp)
+                        ),
+                    interactionSource = remember { MutableInteractionSource() },
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_add_plus),
+                        contentDescription = null,
+                        tint = Color.White,
+                        modifier = Modifier.size(32.dp)
+                    )
+
+                }
+
+            }
         }
-        item {
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = stringResource(id = R.string.text_popular_funding),
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = suit
-            )
-            PopularFundingListPager(sampleFundings, navController)
-        }
-        if (user != null) {
+
+    ) { contentPadding ->
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(contentPadding)
+        ) {
+            item {
+                HomeAppBarPager(navController, user)
+                Spacer(modifier = Modifier.height(4.dp))
+                SearchBarItem(navController)
+            }
             item {
                 Spacer(modifier = Modifier.height(16.dp))
                 Text(
-                    text = stringResource(id = R.string.text_my_friends_funding),
+                    text = stringResource(id = R.string.text_popular_funding),
                     fontSize = 24.sp,
                     fontWeight = FontWeight.Bold,
                     fontFamily = suit
                 )
-                FriendFundingListPager(sampleFundings)
+                PopularFundingListPager(sampleFundings, navController)
+            }
+            if (user != null) {
+                item {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = stringResource(id = R.string.text_my_friends_funding),
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = suit
+                    )
+                    FriendFundingListPager(sampleFundings)
+                }
+            }
+            item {
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = stringResource(id = R.string.text_funding_list),
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = suit
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                FundingAllPager(sampleFundings)
             }
         }
-        item {
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = stringResource(id = R.string.text_funding_list),
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = suit
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            FundingAllPager(sampleFundings)
-        }
+
     }
 }
