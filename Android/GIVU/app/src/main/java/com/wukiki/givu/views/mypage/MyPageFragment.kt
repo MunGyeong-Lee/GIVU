0a44d3ea09b6960e467945f1f68ba4fcb2114559
Jang Hong Jun <jhjun99421@gmail.com>
2025-03-28 13:21:02 +0900

    [Android/Feat] .마이페이지 GIVU pay UI 구현 S12P21D107-82

--- Android/GIVU/app/src/main/java/com/wukiki/givu/views/mypage/MyPageFragment.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/views/mypage/MyPageFragment.kt
@@ -2,16 +2,30 @@ package com.wukiki.givu.views.mypage
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import com.wukiki.givu.R
 import com.wukiki.givu.config.BaseFragment
 import com.wukiki.givu.databinding.FragmentMyPageBinding
+import com.wukiki.givu.views.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
 
+    private val mainViewModel: MainViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.composeMyPage.setContent {
+            MyPageScreen()
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        mainViewModel.setBnvState(true)
     }
 }
