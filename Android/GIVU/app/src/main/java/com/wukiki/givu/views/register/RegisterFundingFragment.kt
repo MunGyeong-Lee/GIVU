a49ed139bbf971b1559dc5ca2d762bed4cceab7d
정지원 <littlesam95@naver.com>
2025-03-28 16:58:16 +0900

    [Android/feat] FundingViewModel에 상품 리스트 저장하기 S12P21D107-119

--- Android/GIVU/app/src/main/java/com/wukiki/givu/views/register/RegisterFundingFragment.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/views/register/RegisterFundingFragment.kt
@@ -1,38 +1,30 @@
 package com.wukiki.givu.views.register
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.fragment.findNavController
-import com.google.accompanist.navigation.animation.AnimatedNavHost
-import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.wukiki.givu.R
 import com.wukiki.givu.config.BaseFragment
 import com.wukiki.givu.databinding.FragmentRegisterFundingBinding
 import com.wukiki.givu.views.MainViewModel
+import com.wukiki.givu.views.detail.viewmodel.FundingViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
-@OptIn(ExperimentalAnimationApi::class)
 @AndroidEntryPoint
-class RegisterFundingFragment : BaseFragment<FragmentRegisterFundingBinding>(R.layout.fragment_register_funding) {
+class RegisterFundingFragment :
+    BaseFragment<FragmentRegisterFundingBinding>(R.layout.fragment_register_funding) {
 
+    private val viewModel: FundingViewModel by activityViewModels()
     private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.vm = viewModel
 
         binding.composeRegisterFunding.setContent {
 
@@ -88,21 +80,19 @@ class RegisterFundingFragment : BaseFragment<FragmentRegisterFundingBinding>(R.l
                 }
 
                 composable("SelectPresent") {
-                    SelectPresentScreen(navController, xmlNavController = findNavController())
+                    SelectPresentScreen(navController = navController, xmlNavController = findNavController())
 
                 }
                 composable("DetailPresent") {
                     DetailPresentScreen(navController, xmlNavController = findNavController())
                 }
             }
-
         }
-
     }
 
     override fun onResume() {
         super.onResume()
+
         mainViewModel.setBnvState(false)
     }
-
 }
