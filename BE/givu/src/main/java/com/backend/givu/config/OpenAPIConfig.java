4a33d2d5024c1687a156e817dc28dc9f0cb6df91
정도현 <dnen14@naver.com>
2025-03-28 10:09:47 +0900

    [BE/fix] swagger 주소 추가

--- BE/givu/src/main/java/com/backend/givu/config/OpenAPIConfig.java
+++ BE/givu/src/main/java/com/backend/givu/config/OpenAPIConfig.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 //public class OpenAPIConfig {
@@ -32,6 +35,10 @@ public class OpenAPIConfig {
                         .title("GIVU API")
                         .description("GIVU 서비스의 API 명세서입니다.")
                         .version("v1.0.0"))
+                .servers(List.of(
+                        new Server().url("https://j12d107.p.ssafy.io/api").description("EC2 Server"),
+                        new Server().url("http://192.168.100.203:8080/api").description("Local Server")
+                                .description("Production server (HTTPS)")))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new io.swagger.v3.oas.models.Components()
                         .addSecuritySchemes(securitySchemeName,
