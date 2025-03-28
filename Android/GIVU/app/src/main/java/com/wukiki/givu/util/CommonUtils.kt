5bfd7c737f906bb2976f1be626ae72f5b571b674
정지원 <littlesam95@naver.com>
2025-03-28 14:47:44 +0900

    [Android/feat] 시간대 변경 및 Formatter 함수 추가 S12P21D107-76

--- Android/GIVU/app/src/main/java/com/wukiki/givu/util/CommonUtils.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/util/CommonUtils.kt
@@ -3,10 +3,10 @@ package com.wukiki.givu.util
 import java.text.DecimalFormat
 
 object CommonUtils {
-    //천단위 콤마
+
+    // 천단위 콤마
     fun makeCommaPrice(num:Int):String{
         val comma = DecimalFormat("#,###")
         return "${comma.format(num)}원"
     }
-
 }
