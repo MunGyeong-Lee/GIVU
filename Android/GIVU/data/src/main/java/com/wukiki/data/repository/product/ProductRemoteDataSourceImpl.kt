cffe42a5f6f14c4ae8a203456b4ff059e2b3bc9e
정지원 <littlesam95@naver.com>
2025-03-28 14:47:09 +0900

    [Android/feat] Product 관련 로직 추가 S12P21D107-76
     - API, Remote Data Source, Repository, Dto 추가

---
+++ Android/GIVU/data/src/main/java/com/wukiki/data/repository/product/ProductRemoteDataSourceImpl.kt
@@ -0,0 +1,21 @@
+package com.wukiki.data.repository.product
+
+import com.wukiki.data.api.ProductApi
+import com.wukiki.data.entity.ProductEntity
+import com.wukiki.data.entity.ProductImageEntity
+import retrofit2.Response
+import javax.inject.Inject
+
+class ProductRemoteDataSourceImpl @Inject constructor(
+    private val productApi: ProductApi
+) : ProductRemoteDataSource {
+
+    override suspend fun getProductDetail(productId: Int): Response<ProductEntity> =
+        productApi.getProductDetail(productId)
+
+    override suspend fun putProductImage(productId: Int): Response<ProductImageEntity> =
+        productApi.putProductImage(productId)
+
+    override suspend fun getProducts(): Response<List<ProductEntity>> =
+        productApi.getProducts()
+}
