649187e539adf3586c6599eebf488e54efe0ac2a
이문경 <moon943380@gmail.com>
2025-03-31 01:57:42 +0900

    [BE/feat] review create api

--- BE/givu/src/main/java/com/backend/givu/model/entity/Review.java
+++ BE/givu/src/main/java/com/backend/givu/model/entity/Review.java
@@ -1,5 +1,6 @@
 package com.backend.givu.model.entity;
 
+import com.backend.givu.model.responseDTO.ReviewsDTO;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -50,5 +51,20 @@ public class Review {
 
     @Column(name = "visit")
     private Long visit;
+
+    public static Review from(User user, Funding funding, ReviewsDTO dto){
+         Review review = Review.builder()
+                .funding(funding)
+                .user(user)
+                .comment(dto.getComment())
+                .image(dto.getImage())
+                .visit(0L)
+                .build();
+
+         return review;
+
+    }
+    public static ReviewsDTO toDTO(Review review) {return new ReviewsDTO(review); }
+
 
 }
