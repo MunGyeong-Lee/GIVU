35a55e54dbd307ddea75fc5c54e156913e06b45e
이문경 <moon943380@gmail.com>
2025-04-01 10:33:58 +0900

    [BE/feat] fundingUpdate-ing

--- BE/givu/src/main/java/com/backend/givu/controller/FundingController.java
+++ BE/givu/src/main/java/com/backend/givu/controller/FundingController.java
@@ -157,10 +157,23 @@ public class FundingController implements FundingControllerDocs {
         fundingService.deleteFunding(userId, fundingId);
         return ResponseEntity.noContent().build(); //204 No Content
     }
-
-
-
-
+//
+//    @Operation(summary = "펀딩 상세보기", description = "해당 펀딩 상세를 보여줍니다.")
+//    @GetMapping(value="/{fundingId}")
+//    public ResponseEntity<FundingDetailDTO> fundingDetail(
+//            @AuthenticationPrincipal CustomUserDetail userDetail,
+//            @PathVariable int fundingId,
+//            HttpServletRequest request) throws IOException{
+//
+//        Long userId = userDetail.getId();
+//
+//        FundingDetailDTO fundingDetail = fundingService.fundingDetail(fundingId);
+//        return ResponseEntity.ok(fundingDetail);
+//    }
+//
+//
+//
+//
 
 
 
