08c4bb90dd36c7c31ec077a15e696ad31b43d3fb
정도현 <dnen14@naver.com>
2025-04-02 11:27:24 +0900

    [BE/feat] ApiResponse 추가

--- BE/givu/src/main/java/com/backend/givu/controller/MyPageController.java
+++ BE/givu/src/main/java/com/backend/givu/controller/MyPageController.java
@@ -5,17 +5,16 @@ import com.amazonaws.Response;
 import com.backend.givu.model.entity.CustomUserDetail;
 import com.backend.givu.model.entity.User;
 import com.backend.givu.model.repository.UserRepository;
+import com.backend.givu.model.responseDTO.ApiResponse;
 import com.backend.givu.model.responseDTO.CodeMessageDTO;
 import com.backend.givu.model.service.MyPageService;
+import com.backend.givu.model.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "MyPage", description = "MyPage 관련 API")
 @RestController
@@ -24,16 +23,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyPageController {
 
     private final MyPageService myPageService;
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @Operation(summary = "유저 연동계좌 생성", description = "해당 유저의 연동계좌를 생성합니다.")
     @PostMapping("/createAccount")
-    public ResponseEntity<CodeMessageDTO> createDepositAccount(@AuthenticationPrincipal CustomUserDetail userDetail){
+    public ResponseEntity<ApiResponse> createDepositAccount(@AuthenticationPrincipal CustomUserDetail userDetail){
         Long userId = userDetail.getId();
-        User user = userRepository.findById(userId).orElseThrow();
+        User user = userService.getUserById(userId);
         if(user.getAccountNumber() != null){
-            return ResponseEntity.ok(new CodeMessageDTO("0001","이미 연동 계좌가 존재합니다."));
+            return ResponseEntity.ok(ApiResponse.fail("ERROR", "이미 연동 계좌가 존재합니다."));
         }
         return ResponseEntity.ok(myPageService.createDepositAccount(user));
     }
+
+    @GetMapping("/checkAccount")
+    public ResponseEntity<> checkAccount(@AuthenticationPrincipal CustomUserDetail userDetail){
+        String accountNo = userDetail.getAccountNo();
+        if(accountNo.isEmpty()){
+           return ResponseEntity.ok().build();
+        }
+
+
+
+    }
+
 }
---
+++ BE/givu/src/main/java/com/backend/givu/model/responseDTO/CheckAccountResponse.java
@@ -0,0 +1,44 @@
+package com.backend.givu.model.responseDTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CheckAccountResponse {
+    @JsonProperty("Header")
+    private Header Header;
+
+    @JsonProperty("REC")
+    private Rec REC;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Header {
+        private String responseCode;
+        private String responseMessage;
+        private String apiName;
+        private String transmissionDate;
+        private String transmissionTime;
+        private String institutionCode;
+        private String apiKey;
+        private String apiServiceCode;
+        private String institutionTransactionUniqueNo;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Rec {
+        private String bankCode;
+        private String accountNo;
+        private String accountBalance;
+        private String accountCreatedDate;
+        private String accountExpiryDate;
+        private String lastTransactionDate;
+        private String currency;
+    }
+}
