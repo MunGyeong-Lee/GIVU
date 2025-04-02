90ee967a3cb2c42ffe8ca1194fda7ed2db2895a9
정도현 <dnen14@naver.com>
2025-04-02 17:04:59 +0900

    [BE/feat] ssafy 계좌에서 기뷰페이 충전 기능 구현

--- BE/givu/src/main/java/com/backend/givu/model/service/MyPageService.java
+++ BE/givu/src/main/java/com/backend/givu/model/service/MyPageService.java
@@ -1,75 +1,122 @@
 package com.backend.givu.model.service;
 
-import com.backend.givu.config.RestTemplateConfig;
 import com.backend.givu.model.entity.User;
-import com.backend.givu.model.requestDTO.CheckAccountRequest;
+import com.backend.givu.model.requestDTO.CheckAccountRequestDTO;
 import com.backend.givu.model.requestDTO.CreateDepositAccountRequest;
-import com.backend.givu.model.responseDTO.ApiResponse;
-import com.backend.givu.model.responseDTO.CheckAccountResponse;
-import com.backend.givu.model.responseDTO.DepositAccountResponse;
-import com.backend.givu.model.responseDTO.UserAccountDTO;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.backend.givu.model.requestDTO.WithdrawalRequestDTO;
+import com.backend.givu.model.responseDTO.*;
+import com.backend.givu.util.ErrorResponseParser;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @Service
 @RequiredArgsConstructor
 public class MyPageService {
 
+    private final WebClient webClient;
     private final RestTemplate restTemplate;
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final ErrorResponseParser errorResponseParser;
 
     @Transactional
-    public ApiResponse<Void> createDepositAccount(User user){
+    public ApiResponse<Void> createDepositAccount(User user) {
 
         String url = "https://finopenapi.ssafy.io/ssafy/api/v1/edu/demandDeposit/createDemandDepositAccount";
 
         CreateDepositAccountRequest requestBody = new CreateDepositAccountRequest();
 
-        ResponseEntity<DepositAccountResponse> response = restTemplate.postForEntity(
-                url,
-                requestBody,
-                DepositAccountResponse.class
-        );
+        DepositAccountResponse body;
 
-        DepositAccountResponse body = response.getBody();
+        try {
+            body = webClient
+                    .post()
+                    .uri(url)
+                    .bodyValue(requestBody)
+                    .retrieve()
+                    .bodyToMono(DepositAccountResponse.class)
+                    .block(); // 동기적으로 응답 받을 때까지 대기
+        } catch (WebClientResponseException e) {
+            String errbody = e.getResponseBodyAsString();
+            String message = errorResponseParser.extractMessage(errbody);
+            return ApiResponse.fail("ERROR", message);
+        } catch (Exception e) {
+            return ApiResponse.fail("ERROR", "외부 API 요청 실패: " + e.getMessage());
+        }
 
-        if (body.getHeader().getResponseCode().equals("H0000")) {
+        if (body != null && "H0000".equals(body.getHeader().getResponseCode())) {
             user.setAccountNumber(body.getREC().getAccountNo());
             return ApiResponse.success(null);
         } else {
             return ApiResponse.fail(
-                    "ERROR",
-                    body.getHeader().getResponseMessage()
+                    " ERROR",
+                    body != null ? body.getHeader().getResponseMessage() : "응답 없음"
             );
         }
     }
 
-    public ApiResponse<UserAccountDTO> checkAccount(String accountNo){
+
+    public ApiResponse<UserAccountDTO> checkAccount(String accountNo) {
         String url = "https://finopenapi.ssafy.io/ssafy/api/v1/edu/demandDeposit/inquireDemandDepositAccountBalance";
-        CheckAccountRequest checkAccountRequest = new CheckAccountRequest(accountNo);
+        CheckAccountRequestDTO checkAccountRequest = new CheckAccountRequestDTO(accountNo);
 
-        ResponseEntity<CheckAccountResponse> response = restTemplate.postForEntity(
-                url,
-                checkAccountRequest,
-                CheckAccountResponse.class
-        );
+        CheckAccountResponseDTO body;
 
-        CheckAccountResponse body = response.getBody();
+        try {
+            body = webClient
+                    .post()
+                    .uri(url)
+                    .bodyValue(checkAccountRequest)
+                    .retrieve()
+                    .bodyToMono(CheckAccountResponseDTO.class)
+                    .block(); // 동기적으로 응답 받을 때까지 대기
+        } catch (WebClientResponseException e) {
+            String errbody = e.getResponseBodyAsString();
+            String message = errorResponseParser.extractMessage(errbody);
+            return ApiResponse.fail("ERROR", message);
+        } catch (Exception e) {
+            return ApiResponse.fail("ERROR", "외부 API 요청 실패: " + e.getMessage());
+        }
 
-        if(body.getHeader().getResponseCode().equals("H0000")){
+        if (body != null && body.getHeader().getResponseCode().equals("H0000")) {
             return ApiResponse.success(new UserAccountDTO(body.getREC().getAccountNo(), Integer.parseInt(body.getREC().getAccountBalance())));
-        }else{
-            return ApiResponse.fail("ERROR", body.getHeader().getResponseMessage());
+        } else {
+            return ApiResponse.fail("ERROR", body != null ? body.getHeader().getResponseMessage() : "응답 없음");
+        }
+    }
+
+    @Transactional
+    public ApiResponse<ChargeResultDTO> withdrawalFromAccount(String accountNo, User user, int amount) {
+        String url = "https://finopenapi.ssafy.io/ssafy/api/v1/edu/demandDeposit/updateDemandDepositAccountWithdrawal";
+        WithdrawalRequestDTO withdrawalRequestDTO = new WithdrawalRequestDTO(accountNo, String.valueOf(amount));
+
+        WithdrawalResponseDTO body;
+
+        try {
+            body = webClient
+                    .post()
+                    .uri(url)
+                    .bodyValue(withdrawalRequestDTO)
+                    .retrieve()
+                    .bodyToMono(WithdrawalResponseDTO.class)
+                    .block(); // 동기적으로 응답 받을 때까지 대기
+        } catch (WebClientResponseException e) {
+            String errbody = e.getResponseBodyAsString();
+            String message = errorResponseParser.extractMessage(errbody);
+            return ApiResponse.fail("ERROR", message);
+        } catch (Exception e) {
+            return ApiResponse.fail("ERROR", "외부 API 요청 실패: " + e.getMessage());
+        }
+
+        if (body != null && body.getHeader().getResponseCode().equals("H0000")) {
+            ApiResponse<UserAccountDTO> res = checkAccount(accountNo);
+            int givupaybalance = res.getData().getBalance();
+            user.setBalance(user.getBalance() + amount);
+            return ApiResponse.success(new ChargeResultDTO(user.getId(), givupaybalance, user.getBalance()));
+        } else {
+            return ApiResponse.fail("ERROR", body != null ? body.getHeader().getResponseMessage() : "응답 없음");
         }
 
     }
