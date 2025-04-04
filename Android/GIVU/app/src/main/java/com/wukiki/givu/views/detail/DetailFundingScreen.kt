0165d0e7ffff6f9019ee07eb7a3b99d97222df79
정지원 <littlesam95@naver.com>
2025-04-04 15:17:43 +0900

    [Android/feat] 펀딩 상세 조회 페이지 개선 S12P21D107-168
     - 펀딩 이미지가 없다면 대체 이미지 표시(임시로 로고를 띄워놓음)
     - 축하 편지 작성자 프로필 사진 출력

--- Android/GIVU/app/src/main/java/com/wukiki/givu/views/detail/DetailFundingScreen.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/views/detail/DetailFundingScreen.kt
@@ -2,6 +2,7 @@ package com.wukiki.givu.views.detail
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,46 +14,90 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.wukiki.givu.R
 import com.wukiki.givu.util.StoreDetailBottomButton
 import com.wukiki.givu.views.detail.component.DetailFundingContent
 import com.wukiki.givu.views.detail.viewmodel.FundingViewModel
+import timber.log.Timber
 
 @Composable
 fun DetailFundingScreen(
     fundingViewModel: FundingViewModel,
     navController: NavController
 ) {
+    val user by fundingViewModel.user.collectAsState()
     val funding by fundingViewModel.selectedFunding.collectAsState()
     val letters by fundingViewModel.selectedFundingLetter.collectAsState()
 
-    Scaffold(
-        bottomBar = {
-            Surface(
-                modifier = Modifier.fillMaxWidth(),
-                shadowElevation = 16.dp,
-                color = Color.White
-            ) {
-                StoreDetailBottomButton(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(68.dp),
-                    text = "이 상품 선택하기",
-                    navController = navController,
-                    actionId = R.id.action_detail_funding_to_participate_funding
-                ) { }
+    funding?.let {
+        Scaffold(
+            bottomBar = {
+                Surface(
+                    modifier = Modifier.fillMaxWidth(),
+                    shadowElevation = 16.dp,
+                    color = Color.White
+                ) {
+                    if (it.writerId == (user?.id ?: "-1").toInt()) {
+                        when (it.fundedAmount == it.productPrice.toInt()) {
+                            true -> {
+                                Timber.d("내가 만든 완료된 펀딩")
+                                StoreDetailBottomButton(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(68.dp),
+                                    text = stringResource(R.string.text_funding_finish),
+                                    navController = navController,
+                                    actionId = R.id.action_detail_funding_to_finish_funding
+                                ) { }
+                            }
+
+                            else -> {
+                                Timber.d("내가 만든 대기중인 펀딩")
+                                Row {
+                                    StoreDetailBottomButton(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .height(68.dp),
+                                        text = stringResource(R.string.text_funding_update),
+                                        navController = navController,
+                                        actionId = R.id.action_detail_funding_to_update_funding
+                                    ) { }
+                                    StoreDetailBottomButton(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .height(68.dp),
+                                        text = stringResource(R.string.text_funding_cancel),
+                                        navController = navController,
+                                        actionId = R.id.action_detail_funding_to_cancel_funding
+                                    ) { }
+                                }
+                            }
+                        }
+                    } else {
+                        Timber.d("다른 사람의 펀딩")
+                        StoreDetailBottomButton(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(68.dp),
+                            text = stringResource(R.string.title_participate_funding),
+                            navController = navController,
+                            actionId = R.id.action_detail_funding_to_participate_funding
+                        ) { }
+                    }
+                }
             }
-        }
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White)
-                .padding(paddingValues)
-        ) {
-            funding?.let { DetailFundingContent(fundingViewModel, it, letters) }
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+                    .padding(paddingValues)
+            ) {
+                DetailFundingContent(fundingViewModel, it, letters)
+            }
         }
     }
 }
--- Android/GIVU/app/src/main/java/com/wukiki/givu/views/detail/component/LetterListPager.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/views/detail/component/LetterListPager.kt
@@ -1,8 +1,10 @@
 package com.wukiki.givu.views.detail.component
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -15,6 +17,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -52,19 +55,42 @@ fun LetterListPager(
                 fontWeight = FontWeight.Bold,
                 fontFamily = suit
             )
-            SortButton(
-                modifier = Modifier,
-                category = letterSort
-            ) {
-                showSheet.value = true
+            if (letters.isNotEmpty()) {
+                SortButton(
+                    modifier = Modifier,
+                    category = letterSort
+                ) {
+                    showSheet.value = true
+                }
             }
         }
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        letters.forEach { letter ->
-            LetterItem(letter)
-            DottedDivider()
+        when (letters.isEmpty()) {
+            true -> {
+                Spacer(modifier = Modifier.height(32.dp))
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "작성된 편지가 없습니다.",
+                        fontFamily = suit,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        color = Color.DarkGray
+                    )
+                }
+            }
+
+            else -> {
+                letters.forEach { letter ->
+                    LetterItem(letter)
+                    DottedDivider()
+                }
+            }
         }
     }
 
