8debed923fe3ea5929b3b7b7dc672f632474a696
Jang Hong Jun <jhjun99421@gmail.com>
2025-04-04 17:54:13 +0900

    [Android/Fix] 홈화면 중복스크롤 수정 S12P21D107-170

--- Android/GIVU/app/src/main/java/com/wukiki/givu/views/home/HomeScreen.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/views/home/HomeScreen.kt
@@ -10,18 +10,26 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
@@ -40,6 +48,7 @@ import com.wukiki.givu.views.home.component.PopularFundingListPager
 import com.wukiki.givu.views.home.component.SearchBarItem
 import com.wukiki.givu.views.home.viewmodel.HomeUiEvent
 import com.wukiki.givu.views.home.viewmodel.HomeViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(
@@ -69,19 +78,23 @@ fun HomeScreen(
 
     Scaffold(
         floatingActionButton = {
-            if (user != null) {
-                Column(
-                    horizontalAlignment = Alignment.End
-                ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                if (user != null) {
                     FloatingActionButton(
-                        onClick = { navController.navigate(R.id.action_home_to_register_funding) },
+                        onClick = {
+                            navController.navigate(R.id.action_home_to_register_funding)
+                        },
                         containerColor = colorResource(id = R.color.main_primary),
                         modifier = Modifier
                             .padding(bottom = 20.dp)
                             .border(
                                 (0.5).dp, Color(0xFFBFE0EF),
                                 RoundedCornerShape(30.dp)
-                            ),
+                            )
+                            .clip(shape = RoundedCornerShape(30.dp)),
                         interactionSource = remember { MutableInteractionSource() },
                     ) {
                         Icon(
@@ -98,6 +111,7 @@ fun HomeScreen(
     ) { contentPadding ->
 
         LazyColumn(
+            
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(contentPadding)
