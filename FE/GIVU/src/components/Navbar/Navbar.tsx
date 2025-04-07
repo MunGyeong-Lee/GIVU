35afcc0a278fcbca3f4bdf293f5ae809d6310ab4
신은찬 <dmscks3126@gmail.com>
2025-04-07 15:56:49 +0900

    [FE] fix: 펀딩 찜하기 기능 전역 삭제

--- FE/GIVU/src/components/Navbar/Navbar.tsx
+++ FE/GIVU/src/components/Navbar/Navbar.tsx
@@ -153,31 +153,17 @@ function Navbar() {
             </nav>
           </div>
 
-          {/* 오른쪽 영역 (프로젝트 올리기, 로그인/프로필) */}
+          {/* 오른쪽 영역 (펀딩 생성하기, 로그인/프로필) */}
           <div className="flex items-center space-x-4">
             <Link
               to="/funding/create"
               className="text-gray-700 text-sm font-medium hover:text-primary-color transition-colors"
             >
-              프로젝트 올리기
+              펀딩 생성하기
             </Link>
 
             {isAuthenticated ? (
               <div className="flex items-center space-x-4">
-                {/* 하트 아이콘 */}
-                <Link to="/likes" className="text-gray-600 hover:text-primary-color">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                  </svg>
-                </Link>
-
-                {/* 알림 아이콘 */}
-                <Link to="/notifications" className="text-gray-600 hover:text-primary-color">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                  </svg>
-                </Link>
-
                 {/* 프로필 드롭다운 */}
                 <div className="relative" ref={profileMenuRef}>
                   <button
