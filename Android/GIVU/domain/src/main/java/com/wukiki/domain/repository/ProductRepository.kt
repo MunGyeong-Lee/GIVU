fb3a13e857d0cfa43d45841278100c6517d4c70c
정지원 <littlesam95@naver.com>
2025-04-08 17:01:26 +0900

    [Android/feat] 상품에 대한 좋아요 기능(Domain 계층) S12P21D107-182

--- Android/GIVU/domain/src/main/java/com/wukiki/domain/repository/ProductRepository.kt
+++ Android/GIVU/domain/src/main/java/com/wukiki/domain/repository/ProductRepository.kt
@@ -2,12 +2,17 @@ package com.wukiki.domain.repository
 
 import com.wukiki.domain.model.ApiResult
 import com.wukiki.domain.model.Product
+import com.wukiki.domain.model.ProductDetail
 
 interface ProductRepository {
 
-    suspend fun getProductDetail(productId: Int): ApiResult<Product>
+    suspend fun getProductDetail(productId: Int): ApiResult<ProductDetail>
 
     suspend fun putProductImage(productId: Int): ApiResult<String>
+
+    suspend fun postProductLike(productId: Int): ApiResult<Unit>
+
+    suspend fun postProductLikeCancel(productId: Int): ApiResult<Unit>
 
     suspend fun getProducts(): ApiResult<List<Product>>
 }
--- Android/GIVU/domain/src/main/java/com/wukiki/domain/usecase/GetProductUseCase.kt
+++ Android/GIVU/domain/src/main/java/com/wukiki/domain/usecase/GetProductUseCase.kt
@@ -2,7 +2,7 @@ package com.wukiki.domain.usecase
 
 import com.wukiki.domain.model.ApiResult
 import com.wukiki.domain.model.Product
-import com.wukiki.domain.model.Review
+import com.wukiki.domain.model.ProductDetail
 import com.wukiki.domain.repository.ProductRepository
 import javax.inject.Inject
 
@@ -13,8 +13,14 @@ class GetProductUseCase @Inject constructor(
     suspend fun getProducts(): ApiResult<List<Product>> =
         productRepository.getProducts()
 
-    suspend fun getProductDetail(productId: Int): ApiResult<Product> =
+    suspend fun getProductDetail(productId: Int): ApiResult<ProductDetail> =
         productRepository.getProductDetail(productId)
+
+    suspend fun likeProduct(productId: Int): ApiResult<Unit> =
+        productRepository.postProductLike(productId)
+
+    suspend fun cancelLikeProduct(productId: Int): ApiResult<Unit> =
+        productRepository.postProductLikeCancel(productId)
 
     suspend fun updateProductImage(productId: Int): ApiResult<String> =
         productRepository.putProductImage(productId)
