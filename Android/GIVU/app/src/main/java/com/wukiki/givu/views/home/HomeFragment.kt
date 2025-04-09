96a1a600fbf31da2110d8073b92edfd0008f33d3
정지원 <littlesam95@naver.com>
2025-04-09 15:19:17 +0900

    [Android/feat] 홈 화면으로 오면 검색 결과 초기화 S12P21D107-200

--- Android/GIVU/app/src/main/java/com/wukiki/givu/views/home/HomeFragment.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/views/home/HomeFragment.kt
@@ -9,6 +9,8 @@ import com.wukiki.givu.config.BaseFragment
 import com.wukiki.givu.databinding.FragmentHomeBinding
 import com.wukiki.givu.views.MainViewModel
 import com.wukiki.givu.views.home.viewmodel.HomeViewModel
+import com.wukiki.givu.views.mall.viewmodel.MallViewModel
+import com.wukiki.givu.views.register.viewmodel.RegisterViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,6 +18,8 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     private val viewModel: HomeViewModel by activityViewModels()
     private val mainViewModel: MainViewModel by activityViewModels()
+    private val registerViewModel: RegisterViewModel by activityViewModels()
+    private val mallViewModel: MallViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -34,5 +38,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
         mainViewModel.setBnvState(true)
         viewModel.initUserInfo()
+        registerViewModel.setFromMall(false)
+        mallViewModel.initProducts()
     }
 }
