fb02d06288a53c92d7e6877c9d2bb4f96c00e2ae
정도현 <dnen14@naver.com>
2025-04-09 15:07:12 +0900

    [BE/feat] 상품 구매 구현

--- BE/givu/src/main/java/com/backend/givu/kafka/GivuProducer.java
+++ BE/givu/src/main/java/com/backend/givu/kafka/GivuProducer.java
@@ -2,19 +2,20 @@ package com.backend.givu.kafka;
 
 import com.backend.givu.model.entity.Payment;
 import com.backend.givu.model.requestDTO.GivuTransferEventDTO;
+import com.backend.givu.model.requestDTO.OrderCreatedEventDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class GivuProducer {
 
-
-
-    private final KafkaTemplate<String, GivuTransferEventDTO> kafkaTemplate;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void sendKafkaEventAfterCommit(Payment payment, Long userId) {
         String reason = null;
@@ -30,7 +31,18 @@ public class GivuProducer {
                     }
                 }
         );
+    }
 
+    public void sendKafkaEventAfterCommit(OrderCreatedEventDTO event) {
+        TransactionSynchronizationManager.registerSynchronization(
+                new TransactionSynchronization() {
+                    @Override
+                    public void afterCommit() {
+                        kafkaTemplate.send("order-created-topic", event.getUserId().toString(), event);
+                        log.info("✅ Kafka 결제 요청 이벤트 발행 완료 (after commit) - paymentId: {}", event.getPaymentId());
+                    }
+                }
+        );
     }
 
 
