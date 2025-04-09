3fbbe876a26c299fcfc323834156bc40676f6e19
정지원 <littlesam95@naver.com>
2025-04-09 10:20:41 +0900

    [Android/feat] 펀딩 취소 UI 개선 S12P21D107-197
     - 달성률 50% 이하면 취소 API 호출, 50% 초과하면 완료 API 호출
     - 얼굴 인식으로 취소 가능

--- Android/GIVU/app/src/main/java/com/wukiki/givu/views/cancel/CancelFundingFragment.kt
+++ Android/GIVU/app/src/main/java/com/wukiki/givu/views/cancel/CancelFundingFragment.kt
@@ -2,11 +2,14 @@ package com.wukiki.givu.views.cancel
 
 import android.os.Bundle
 import android.view.View
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.wukiki.givu.R
 import com.wukiki.givu.config.BaseFragment
 import com.wukiki.givu.databinding.FragmentCancelFundingBinding
+import com.wukiki.givu.views.BiometricAuth
 import com.wukiki.givu.views.detail.viewmodel.FundingViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +18,7 @@ class CancelFundingFragment :
     BaseFragment<FragmentCancelFundingBinding>(R.layout.fragment_cancel_funding) {
 
     private val viewModel: FundingViewModel by activityViewModels()
+    private var biometricAuthResult = mutableStateOf(false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -22,7 +26,30 @@ class CancelFundingFragment :
         binding.vm = viewModel
 
         binding.composeCancelFunding.setContent {
-            CancelFundingScreen(viewModel, findNavController())
+            if (biometricAuthResult.value) {
+                LaunchedEffect(Unit) {
+                    biometricAuthResult.value = false
+                    val fundedAmount = viewModel.selectedFunding.value?.fundedAmount ?: 0
+                    val productPrice = (viewModel.selectedFunding.value?.productPrice ?: "0").toInt()
+                    if (fundedAmount * 2 <= productPrice) {
+                        viewModel.cancelFunding()
+                    } else {
+                        viewModel.finishFunding()
+                    }
+                }
+            }
+
+            CancelFundingScreen(viewModel, findNavController()) {
+                BiometricAuth(
+                    activity = requireActivity(),
+                    onSuccess = {
+                        biometricAuthResult.value = true
+                    },
+                    onFailure = {
+                        // 실패 시 처리
+                    }
+                ).authenticate()
+            }
         }
     }
 }
