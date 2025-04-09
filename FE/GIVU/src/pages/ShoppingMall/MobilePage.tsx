979caf37f10c65bf9fd5046ffff51b12d7a76a19
장홍준 <jhjun99421@gmail.com>
2025-04-10 00:27:59 +0900

    롤백

--- FE/GIVU/src/pages/ShoppingMall/MobilePage.tsx
+++ FE/GIVU/src/pages/ShoppingMall/MobilePage.tsx
@@ -278,11 +278,7 @@ const OrderPage = () => {
       if (result.code === 'SUCCESS') {
         alert('결제가 완료되었습니다!');
         // ✅ WebView 종료
-        if (window.Android && typeof window.Android.paymentComplete === 'function') {
-          window.Android.paymentComplete();
-        } else {
-          window.close();
-        }
+        window.close();
       } else {
         throw new Error(result.message || '결제에 실패했습니다.');
       }
